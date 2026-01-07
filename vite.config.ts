195377592a0b51b37869f41f4b2a42f107ad330a
Koteyye <koteyye@yandex.ru>
2026-01-08 00:55:31 +0300

    Фикс деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,17 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
+  const keyPath = path.resolve(__dirname, 'localhost-key.pem');
+  const certPath = path.resolve(__dirname, 'localhost.pem');
+  
+  let httpsConfig = undefined;
+  if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
+      httpsConfig = {
+          key: fs.readFileSync(keyPath),
+          cert: fs.readFileSync(certPath),
+      };
+  }
+
   return {
     plugins: [vue()],
     resolve: {
@@ -21,10 +32,7 @@ export default defineConfig(({ mode }) => {
     server: {
       host: true,
       port: 3000,
-      https: {
-        key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-        cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-      },
+      https: httpsConfig,
       proxy: {
         "/api": {
           target: env.VITE_PROXY_TARGET || "http://localhost:8080",
